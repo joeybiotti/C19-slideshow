e40b3bff8f04796779ef95b54528a7286d0df95e
Joey Biotti <joey@MacBook-Pro.local>
2017-05-30 14:15:59 -0500

    added dom builder js and stuff

--- javascripts/dom-builder.js
+++ javascripts/dom-builder.js
@@ -1,3 +1,11 @@
 "use strict";
 
 console.log("dom builder has loaded");
+
+let $ = require('jquery'),
+	mainTemplate = require('../templates/main.hbs');
+
+function buildDOM(build){
+	let mainOutput = mainTemplate(build);
+	$("#output").html(mainOutput);
+}
